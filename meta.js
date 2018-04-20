226d3e3e9c764b9208322797d7f5465c3f3fcb30
gonglong2018 <2304018971@qq.com>
2018-04-20 11:22:35 +0800

    删除多余提示消息, 修改多页命令

--- meta.js
+++ meta.js
@@ -18,106 +18,106 @@ module.exports = {
       "type": "string",
       "required": false,
       "message": "Project description",
-      "default": "dz vue multi page."
+      "default": "add Multi Page Apps."
     },
     "author": {
       "type": "string",
       "message": "Author"
-    },
-    "build": {
-      "type": "list",
-      "message": "Vue build",
-      "choices": [
-        {
-          "name": "Runtime + Compiler: recommended for most users",
-          "value": "standalone",
-          "short": "standalone"
-        },
-        {
-          "name": "Runtime-only: about 6KB lighter min+gzip, but templates (or any Vue-specific HTML) are ONLY allowed in .vue files - render functions are required elsewhere",
-          "value": "runtime",
-          "short": "runtime"
-        }
-      ]
-    },
-    "router": {
-      "type": "confirm",
-      "message": "Install vue-router?"
-    },
+    }
+    // "build": {
+    //   "type": "list",
+    //   "message": "Vue build",
+    //   "choices": [
+    //     {
+    //       "name": "Runtime + Compiler: recommended for most users",
+    //       "value": "standalone",
+    //       "short": "standalone"
+    //     },
+    //     {
+    //       "name": "Runtime-only: about 6KB lighter min+gzip, but templates (or any Vue-specific HTML) are ONLY allowed in .vue files - render functions are required elsewhere",
+    //       "value": "runtime",
+    //       "short": "runtime"
+    //     }
+    //   ]
+    // },
+    // "router": {
+    //   "type": "confirm",
+    //   "message": "Install vue-router?"
+    // },
     // "vuex": {
     //   "type": 'confirm',
     //   "message": 'Do you wanner install VUEX?'
     // },
-    "lint": {
-      "type": "confirm",
-      "message": "Use ESLint to lint your code?"
-    },
-    "lintConfig": {
-      "when": "lint",
-      "type": "list",
-      "message": "Pick an ESLint preset",
-      "choices": [
-        {
-          "name": "Standard (https://github.com/standard/standard)",
-          "value": "standard",
-          "short": "Standard"
-        },
-        {
-          "name": "Airbnb (https://github.com/airbnb/javascript)",
-          "value": "airbnb",
-          "short": "Airbnb"
-        },
-        {
-          "name": "none (configure it yourself)",
-          "value": "none",
-          "short": "none"
-        }
-      ]
-    },
-    "unit": {
-      "type": "confirm",
-      "message": "Setup unit tests"
-    },
-    "runner": {
-      "when": "unit",
-      "type": "list",
-      "message": "Pick a test runner",
-      "choices": [
-        {
-          "name": "Jest",
-          "value": "jest",
-          "short": "jest"
-        },
-        {
-          "name": "Karma and Mocha",
-          "value": "karma",
-          "short": "karma"
-        },
-        {
-          "name": "none (configure it yourself)",
-          "value": "noTest",
-          "short": "noTest"
-        }
-      ]
-    },
-    "e2e": {
-      "type": "confirm",
-      "message": "Setup e2e tests with Nightwatch?"
-    }
+    // "lint": {
+    //   "type": "confirm",
+    //   "message": "Use ESLint to lint your code?"
+    // },
+    // "lintConfig": {
+    //   "when": "lint",
+    //   "type": "list",
+    //   "message": "Pick an ESLint preset",
+    //   "choices": [
+    //     {
+    //       "name": "Standard (https://github.com/standard/standard)",
+    //       "value": "standard",
+    //       "short": "Standard"
+    //     },
+    //     {
+    //       "name": "Airbnb (https://github.com/airbnb/javascript)",
+    //       "value": "airbnb",
+    //       "short": "Airbnb"
+    //     },
+    //     {
+    //       "name": "none (configure it yourself)",
+    //       "value": "none",
+    //       "short": "none"
+    //     }
+    //   ]
+    // },
+    // "unit": {
+    //   "type": "confirm",
+    //   "message": "Setup unit tests"
+    // },
+    // "runner": {
+    //   "when": "unit",
+    //   "type": "list",
+    //   "message": "Pick a test runner",
+    //   "choices": [
+    //     {
+    //       "name": "Jest",
+    //       "value": "jest",
+    //       "short": "jest"
+    //     },
+    //     {
+    //       "name": "Karma and Mocha",
+    //       "value": "karma",
+    //       "short": "karma"
+    //     },
+    //     {
+    //       "name": "none (configure it yourself)",
+    //       "value": "noTest",
+    //       "short": "noTest"
+    //     }
+    //   ]
+    // },
+    // "e2e": {
+    //   "type": "confirm",
+    //   "message": "Setup e2e tests with Nightwatch?"
+    // }
   },
   "filters": {
-    ".eslintrc.js": "lint",
-    ".eslintignore": "lint",
-    "config/test.env.js": "unit || e2e",
-    "build/webpack.test.conf.js": "e2e || (unit && runner === 'karma')",
-    "test/unit/**/*": "unit",
-    "test/unit/index.js": "unit && runner === 'karma'",
-    "test/unit/jest.conf.js": "unit && runner === 'jest'",
-    "test/unit/karma.conf.js": "unit && runner === 'karma'",
-    "test/unit/specs/index.js": "unit && runner === 'karma'",
-    "test/unit/setup.js": "unit && runner === 'jest'",
-    "test/e2e/**/*": "e2e",
-    "src/router/**/*": "router",
+    // ".eslintrc.js": "lint",
+    // ".eslintignore": "lint",
+    // "config/test.env.js": "unit || e2e",
+    // "build/webpack.test.conf.js": "e2e || (unit && runner === 'karma')",
+    // "test/unit/**/*": "unit",
+    // "test/unit/index.js": "unit && runner === 'karma'",
+    // "test/unit/jest.conf.js": "unit && runner === 'jest'",
+    // "test/unit/karma.conf.js": "unit && runner === 'karma'",
+    // "test/unit/specs/index.js": "unit && runner === 'karma'",
+    // "test/unit/setup.js": "unit && runner === 'jest'",
+    // "test/e2e/**/*": "e2e",
+    // "src/router/**/*": "router",
     // 'src/store/**/*': 'vuex',  //加入自己的目录
   },
   "completeMessage": "To get started:\n\n  {{^inPlace}}cd {{destDirName}}\n  {{/inPlace}}npm install\n  npm run dev\n\nDocumentation can be found at https://vuejs-templates.github.io/webpack"
